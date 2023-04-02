16f0055e67f9b20ab577cdf6e22323397a8e5b4e
yourusername <botaoguo@smail.hunnu.edu.cn>
2023-04-02 19:53:32 +0800

    Update steer file for Varial

--- PlottingWithVarial/steer_botao.sh
+++ PlottingWithVarial/steer_botao.sh
@@ -1,9 +1,20 @@
+# check if there exist Varial
+if [ "$1" == "varial" ]; then
+    echo "Installing Varial..."
+    git clone https://github.com/HeinerTholen/Varial.git
+fi
+
 #usage
-#1.
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.20.08/x86_64-centos7-gcc48-opt/bin/thisroot.sh
-#2. 
-export PYTHONPATH=$PYTHONPATH:/home/pku/botaoguo/workplace/bbmumu/rootdir/Varial-master
-#3.
-export PATH=$PATH:/home/pku/botaoguo/workplace/bbmumu/rootdir/Varial-master/bin
-#4.
+
+if [ $USER == "botaoguo" ]; then
+    export PYTHONPATH=$PYTHONPATH:/home/pku/botaoguo/workplace/bbmumu/rootdir/Varial-master
+    export PATH=$PATH:/home/pku/botaoguo/workplace/bbmumu/rootdir/Varial-master/bin
+else
+    export PYTHONPATH=$PYTHONPATH:$PWD/Varial
+    export PATH=$PATH:$PWD/Varial/bin
+fi
+
 python run_plotting_with_varial.py plot_config_ggF.py
+
+echo "Done!"
